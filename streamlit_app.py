39f4189a5537a4a90e809b24e6ab4c42d90ca71d
doddyz <doddyz@live.com>
2022-05-01 13:00:48 +0100

    4th

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,15 +8,16 @@ st.set_page_config(page_title='Nombres Aléatoires', page_icon=':game_die:', lay
 
 st.markdown('# Nombres :game_die:')
 
-n = st.sidebar.number_input('Max', min_value=2, value=100, step=10)
+columns = st.columns([1.5, 10])
 
-columns = st.columns(2)
-
-refresh_button = st.sidebar.button('Rafraîchir')
+with columns[0]:
+    n = st.number_input('Max', min_value=2, value=100, step=10)
+    refresh_button = st.button('Rafraîchir')
 
 if refresh_button:
     html_header = '<h1 style="font-size:150px">' + str(generate_random_num(n)) + '</h1>'
-    st.markdown(html_header, unsafe_allow_html=True)
+    with columns[1]:
+        st.markdown(html_header, unsafe_allow_html=True)
   
 
             
