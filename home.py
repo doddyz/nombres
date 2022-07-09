5ea2d58abe164a2a0d34191a7a54dfcbfef7443e
doddyz <doddyz@live.com>
2022-07-09 21:17:55 +0100

    Start of multi page with sum operation in Latex display w Home

---
+++ home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.set_page_config(page_title='Nombres Aléatoires', page_icon=':game_die:', layout="wide", initial_sidebar_state="auto", menu_items=None)
+
+st.markdown('# Nombres :game_die:')
